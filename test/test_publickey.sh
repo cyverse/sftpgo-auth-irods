826089797ac928a2cb60583f9d022b350f0fd463
Illyoung Choi <iychoi@email.arizona.edu>
2023-09-13 16:10:26 -0700

    set cs negotiation

--- test/test_publickey.sh
+++ test/test_publickey.sh
@@ -5,9 +5,11 @@ export IRODS_PROXY_PASSWORD=""
 export IRODS_HOST="data.cyverse.org"
 export IRODS_PORT=1247
 export IRODS_ZONE="iplant"
+export IRODS_REQUIRE_CS_NEGOTIATION=true
+export IRODS_CS_NEGOTIATION_POLICY=CS_NEG_DONT_CARE
 export SFTPGO_AUTHD_USERNAME="iychoi"
 export SFTPGO_AUTHD_PASSWORD=""
 export SFTPGO_AUTHD_PUBLIC_KEY="ssh-rsa XXXXX"
 export SFTPGO_AUTHD_IP="10.10.10.10"
 
-../bin/sftpgo-auth-irods
+../bin/sftpgo-auth-irods --fake
--- test/test_publickey_pam_for_users.sh
+++ test/test_publickey_pam_for_users.sh
@@ -6,6 +6,8 @@ export IRODS_HOST="data.cyverse.org"
 export IRODS_PORT=1247
 export IRODS_ZONE="iplant"
 export IRODS_AUTH_SCHEME="pam_for_users"
+export IRODS_REQUIRE_CS_NEGOTIATION=true
+export IRODS_CS_NEGOTIATION_POLICY=CS_NEG_DONT_CARE
 export IRODS_SSL_CA_CERT_PATH="/etc/ssl/certs/ca-certificates.crt"
 export IRODS_SSL_ALGORITHM="AES-256-CBC"
 export IRODS_SSL_KEY_SIZE=32
@@ -16,4 +18,4 @@ export SFTPGO_AUTHD_PASSWORD=""
 export SFTPGO_AUTHD_PUBLIC_KEY="ssh-rsa XXXXX"
 export SFTPGO_AUTHD_IP="10.10.10.10"
 
-../bin/sftpgo-auth-irods
+../bin/sftpgo-auth-irods --fake
